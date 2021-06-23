8d716efdff22b7fca71308432e18f69503ead2e5
KSheeds1 <katiesheedy@hotmail.com>
2021-06-23 14:05:40 +0000

    Calculate surplus data.

--- run.py
+++ run.py
@@ -64,7 +64,7 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully.\n")
 
 
-def calculate_surplus_data(sales_data):
+def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each
     item type. The surplus is defined as the sales figures subtracted
@@ -75,7 +75,13 @@ def calculate_surplus_data(sales_data):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    print(surplus_data)
+
 
 def main():
     """
